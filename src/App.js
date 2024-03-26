9fac37bda20be34d3ddb6304a2c79a543f939622
jparkley <parkleydondon@yahoo.com>
2024-03-26 16:48:53 -0700

    add useMutation of React Query

--- src/App.js
+++ src/App.js
@@ -1,27 +1,60 @@
 import "./App.css";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 
 function App() {
+  const queryClient = useQueryClient();
+
   const { data, error, isLoading } = useQuery({
-    queryKey: ["albums"],
+    queryKey: ["posts"],
     queryFn: () =>
-      fetch("https://jsonplaceholder.typicode.com/albums").then((res) =>
+      fetch("https://jsonplaceholder.typicode.com/posts").then((res) =>
         res.json()
       ),
   });
 
-  console.log("🚀 ~ App ~ data:", data);
+  const { mutate, isError, isPending } = useMutation({
+    mutationFn: (newPost) =>
+      fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newPost),
+      }).then((res) => res.json()),
+    onSuccess: (newPost) => {
+      // queryClient.invalidateQueries({ queryKey: ["posts"] });
+      queryClient.setQueriesData(["posts"], (oldPosts) => [
+        ...oldPosts,
+        newPost,
+      ]);
+    },
+  });
 
-  if (isLoading) return <h1>... Loading</h1>;
-  if (error) {
-    return <pre>{JSON.stringify(error)}</pre>;
+  if (isLoading) return <h1>Loading...</h1>;
+  if (error || isError) {
+    return <pre>There is an error</pre>;
   }
 
   return (
     <div className="App">
-      <h2>List </h2>
-      {data?.map((post) => {
-        return <div key={post.id}>{post.title}</div>;
+      <h2>Posts </h2>
+      {isPending && <p>Post is being added...</p>}
+      <button
+        onClick={() =>
+          mutate(
+            { id: 999, userId: 1000, title: "New Post", body: "HELLO WORLD" } // for test
+          )
+        }
+      >
+        Add New Post
+      </button>
+
+      {data?.map((post, idx) => {
+        return (
+          <div key={post.id}>
+            {/* <h2>User: {post.userId}</h2> */}
+            No.{idx + 1} - {post.title}
+            <p>{post.body}</p>
+          </div>
+        );
       })}
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import "./index.css";
 import App from "./App";
 
@@ -11,6 +13,7 @@ root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
 );
